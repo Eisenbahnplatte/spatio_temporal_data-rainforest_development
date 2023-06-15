700603293d36fd752b2fe96a76bdcfec1b09d066
dp913ljuu <dp913ljuu@clara10.sc.uni-leipzig.de>
2023-06-15 16:14:10 +0200

    Adds sampling lib
    Also better gitignore
    And better Notebook

--- rainforest_lib.jl
+++ rainforest_lib.jl
@@ -6,11 +6,50 @@ module RainforestLib
 
     DotEnv.config()
 
+    sample_cube_dir = "./sample_cubes"
 
-    function load_full_datacube()
-        c1_zarr = Zarr.zopen(ENV["ZARR_PATH"])
-        c1_dataset = YAXArrays.open_dataset(c1_zarr)
-        return YAXArrays.Cube(c1_dataset)
+    
+    
+    function get_lcc_datacube()
+        zarr = Zarr.zopen(ENV["ZARR_PATH"])
+        lc_dataset = YAXArrays.open_dataset(zarr)
+        return lc_dataset[:lccs_class]
+    end
+
+
+    function rough_spatial_filter(cube, lon_bounds = (30,90), lat_bounds = (-30, 15), time_bounds = (Date(2010), Date(2012)))
+
+            return cube[lon = lon_bounds, lat = lat_bounds, time = time_bounds]
+    end
+
+    
+
+    function save_samplecube(cube, name="testcube.zarr", override=false)
+        
+        pathname = string(sample_cube_dir, "/", name) 
+        
+        if !isfile(pathname)
+            println("Saving the sample cube to disk...")
+            
+            YAXArrays.savecube(cube, pathname, driver=:zarr)
+        elseif override
+            println("Overriding existing sample cube....")
+            YAXArrays.savecube(cube, pathname, driver=:zarr)
+        end
+    
+    end
+    
+    function get_samplecube()
+        
+        if !isfile(testing_cube_path)
+            
+            println("Sample cube not available, building....")
+            build_and_save_samplecube()
+        end
+        
+        zarr = Zarr.zopen(testing_cube_path)
+        return YAXArrays.Cube(YAXArrays.open_dataset(zarr))
+    
     end
 
 end
